367f4cb0f3b3c9dd072cbef4323280cfdb97f6b1
achchudhan <123426212+achchudhan@users.noreply.github.com>
2023-03-07 22:31:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,283 @@
+
+import './App.css';
+
+
+    function App() {
+      return (
+        <>
+          <nav class="navbar is-light is-fixed-top" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+      </a>
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+     
+    </div>
+  </nav>
+  
+  
+    <section class="hero is-light block py-6 custom-font">
+        
+        <div class="hero-body">
+      <div class="column is-4 mx-auto">
+        <div class="title-underline"></div>
+      </div>
+
+      
+      
+      <div class="columns mx-0">
+       
+              <div class="navbar-end">
+                  <div class="navbar-item">
+                    <div class="buttons">
+                        
+                        <aside class="menu is-fixed-top">
+                            <ul class="menu-list">
+                                <li><a href="#intro" class="button is-link">Intro</a></li>
+                                <li><a href="#edu" class="button is-link">Education</a></li>
+                                <li><a href="#project" class="button is-link">Projects</a></li>
+                                <li><a href="#contact" class="button is-link">Contacts</a></li>
+                            </ul>
+                        </aside>
+                        </div>
+                        <br />
+                      
+                    
+                  </div>
+                </div>
+                <div class="column is-5 m-auto p-6 ">
+                    <p data-aos="fade-up">
+                     
+                      <h1 class="title is-2 ml-6 has-text-black">HI ! </h1>
+                      <h2 class="title is-2 ml-6 has-text-black">I Am </h2>
+                      <h1 class="title is-1 ml-6 has-text-black is-family-monospace">ACHCHUTHAN</h1>
+                      <h2 class="title is-2 ml-6 has-text-danger has-text-weight-bold">A Creative Designer</h2>
+                     
+                    </p>
+                  </div>
+            </div>
+        <figure data-aos="fade-down" class="image p-6 m-auto column is-5">
+            <img class="is-rounded" width='400' height='400' src="achchu.jpg" alt="" />
+           
+          </figure>
+       
+        
+        </div>
+      
+    </section>
+
+    <div id="intro" class="pt-6"></div>
+    <section class="hero  has-background-dark block py-6 custom-font" >
+        <div class="column is-4 mx-auto">
+            <h2 data-aos="fade-out" class="title has-text-centered is-uppercase">
+                <span class="icon">
+                    <i class="fas fa-info"></i>
+                    </span>
+              About me!
+            </h2>
+            <div class="title-underline"></div>
+          </div>
+        <div class="hero-body">
+      <div class="column is-4 mx-auto">
+        
+        <div class="title-underline"></div>
+      </div>
+      <div class="columns mx-0">
+        
+       
+        <div class="column is-5 m-auto p-6 ">
+            <p data-aos="fade-up">
+             <h2 class="title is-3 has-text-blue " >Highly creative and multitalented Graphic Designer with extensive experience in
+                multimedia, marketing, and print design.Passionate and inventive creator of
+                innovative marketing strategies and campaigns. <br/><br/>
+                
+                
+                I am a very organized person who likes to be kept busy and very eager to learn new
+                skills as well as work well in a team, as well as on my own initiative and also
+                enthusiastic and like to work hard to ensure that I am successful in everything that
+                I do.
+                </h2><br/>
+              
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <div id="edu" class="pt-6"></div>
+    <section class="hero  has-background-light block py-6 custom-font">
+        <div class="column is-4 mx-auto">
+            <h2 data-aos="fade-out" class="title has-text-centered is-uppercase">
+                <span class="icon">
+                    <i class="fas fa-book"></i>
+                    </span>
+              Educational Qualifications
+            </h2>
+            <div class="title-underline"></div>
+          </div>
+        <div class="hero-body">
+      <div class="column is-4 mx-auto">
+        
+        <div class="title-underline"></div>
+      </div>
+      <div class="columns mx-0">
+        
+       
+        <div class="column is-5 m-auto p-6 ">
+            <p data-aos="fade-up">
+             <h2 class="title is-3 has-text-black" ></h2>
+ 
+                
+    <div class="container">
+        <div class="columns">
+          <div class="column">
+            <div class="box">
+              <div class="content">
+                <h4 class="title is-5">2022</h4>
+                
+                Bachelor of Science Hons. in Information Systems
+                (Reading) at University of Colombo School of
+                Computing 
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="box">
+              <div class="content">
+                <h4 class="title is-5">2020</h4>
+                GCE Advanced Level Exam(Bio Science Stream)<br/><br/>
+
+                J/Chavakachcheri Hindu College<br/><br/>
+                
+                Biology - B <br/> Physics - C <br/> Chemistry - C <br/>
+                
+                </div>
+            </div>
+          </div>
+        </div>
+  
+        <div class="columns">
+          <div class="column">
+            <div class="box">
+              <div class="content">
+                <h4 class="title is-5">2015</h4>
+                GCE Ordinary Level Exam <br/> <br/>
+
+                J/Chavakachcheri Hindu College<br/>
+
+English - A<br/>
+
+Mathematics - A <br/>
+                
+                
+              </div>
+            </div>
+          </div>
+          </div>
+       </div> 
+            </p>
+          
+        </div>
+        </div>
+      </div>
+    </section>
+
+
+    <div id="project" class="pt-4"></div>
+    <section class="hero  has-background-light block py-6 custom-font" >
+        <div class="column is-4 mx-auto">
+            <h2 data-aos="fade-out" class="title has-text-centered is-uppercase">
+                <span class="icon">
+                    <i class="fas fa-chart-pie"></i>
+                    </span>
+              PROJECT
+            </h2>
+            <div class="title-underline"></div>
+          </div>
+        <div class="hero-body">
+      <div class="column is-4 mx-auto">
+        
+        <div class="title-underline"></div>
+      </div>
+      <div class="columns mx-0">
+        <div class="column is-5 m-auto p-6 ">
+            <p data-aos="fade-up">
+             <h2 class="title is-3 has-text-black" >
+                
+                <li>A portfolio website</li>
+
+
+                <li>A clone website</li>
+
+
+                <li>A CRUD web application</li>
+            <li>An API-connected website</li>
+        <li>A 'perfect score' website</li> 
+    </h2><br/>
+              
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <div id="contact" class="title pt-6 has-text-white"></div>
+    <section class="hero  has-background-black block py-6 custom-font" >
+        <div class="column is-4 mx-auto">
+            <h2 data-aos="fade-out" class="title has-text-centered is-uppercase">
+                <span class="icon"> 
+                    <i class="fas fa-envelope"></i>
+                    </span>
+            <span >Contact Details</span>     
+             
+            </h2>
+            <div class="title-underline"></div>
+          </div>
+        <div class="hero-body">
+      <div class="column is-5 mx-auto">
+        
+        <div class="title-underline"></div>
+      </div>
+      <div class="columns mx-0">
+        
+       
+        <div class="column is-5 m-auto p-6 ">
+            <p data-aos="fade-up">
+             <h2 class="title is-3 has-text-white" >
+                
+                <span class="icon">
+                    <i class="fas fa-phone"></i>
+                    </span> <li> 0761243323</li><br/><br/>
+
+
+                    <span class="icon">
+                        <i class="fab fa-linkedin"></i>
+                        </span>
+                       <li> 1/2,Rachu Road,Dehiwale</li><br/><br/>
+
+                        <span class="icon">
+                            <i class="fas fa-envelope-square"></i>
+                            </span>          
+                       <li> achchu@yahoo.com</li>
+ </h2><br/>
+              
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+          
+          </>
+      );
+    }
+    
+    export default App;
+    
+  
+
+
